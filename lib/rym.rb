1bba06c4ff5afad59da3be514d38db1c05fda698
Mark Ryall <mark@ryall.name>
2011-11-05 18:45:07 +1000

    added extaction of release date from artist page

--- lib/rym.rb
+++ lib/rym.rb
@@ -3,9 +3,15 @@ require 'cgi'
 
 class Artist
   attr_reader :albums
-
   def initialize albums
     @albums = albums
+  end
+end
+
+class Album
+  attr_reader :title, :date
+  def initialize title, date
+    @title, @date = title, date
   end
 end
 
@@ -24,8 +30,9 @@ class Rym
   def artist name
     @browser.goto "http://rateyourmusic.com/artist/#{name.gsub(' ', '_')}"
     albums = @browser.table(:id => 'album_disc_s').rows[2..-1].map do |r|
-      # date = r.td.span.title
-      r.tds[1].a.text
+      title = r.tds[1].a.text
+      date = parse_date r.td.span.title
+      Album.new title, date
     end
     artist = Artist.new albums
     artist
@@ -33,5 +40,25 @@ class Rym
 
   def close
     @browser.close
+  end
+private
+  MONTHS = %w{Nothing January February March April May June July August September October November December}
+
+  def parse_date s
+    day = 15
+    month = 6
+    year = 2000
+    case s
+    when /(\w+) (\d+), (\d+)/
+      month = MONTHS.index $1
+      day = $2.to_i
+      year = $3.to_i
+    when /(\w+) (\d+)/
+      month = MONTHS.index $1
+      year = $2.to_i
+    when /(\d+)/
+      year = $1.to_i
+    end
+    Date.new year, month, day
   end
 end
--- spec/rym/rym_spec.rb
+++ spec/rym/rym_spec.rb
@@ -7,6 +7,10 @@ describe Rym do
 
   #after { rym.close }
 
+  def d *args
+    Date.new *args
+  end
+
   it 'should search artist' do
     rym.search_artist('sepulture').first.should == 'Sepultura'
   end
@@ -15,7 +19,11 @@ describe Rym do
     rym.search_artist('tmbg').should == ['They Might Be Giants']
   end
 
-  it 'should retrive artist' do
-    rym.artist('they might be giants').albums[3..6].should == ['Lincoln', 'Flood', 'Apollo 18', 'John Henry']
+  it 'should retrive artist album titles' do
+    rym.artist('they might be giants').albums[3..6].map(&:title).should == ['Lincoln', 'Flood', 'Apollo 18', 'John Henry']
+  end
+
+  it 'should retrieve artist album release dates' do
+    rym.artist('they might be giants').albums[3..6].map(&:date).should == [d(1988,11,15), d(1990,1,5), d(1992,3,24), d(1994,9,13)]
   end
 end
